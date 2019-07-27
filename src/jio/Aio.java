f775a6c61744c8b91f48e98ce35617b06048e6dd
sudarshan agolla <sudarshan agolla@DESKTOP-4UR5ELF>
2019-07-27 10:39:23 +0530

    newly added aio class

---
+++ src/jio/Aio.java
@@ -0,0 +1,20 @@
+package jio;
+
+public class Aio {
+	
+	private Integer eno;
+	private String ename;
+	public Integer getEno() {
+		return eno;
+	}
+	public void setEno(Integer eno) {
+		this.eno = eno;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+}
